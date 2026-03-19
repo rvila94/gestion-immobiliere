48ccab237e1d46cac1df4fbf99a476c1737546c6
rvila94 <r94vila@gmail.com>
2026-03-19 16:17:09 +0100

    feat(user): implement UserService and create_user endpoint

--- app/api/routes/users.py
+++ app/api/routes/users.py
@@ -1,7 +1,35 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+from marshmallow import ValidationError
+from app.schemas.user_schema import UserSchema
+from app.services.user_service import UserService
 
-users_bp = Blueprint("users", __name__, url_prefix="/api/users")
+# Définition du Blueprint
+users_bp = Blueprint("user_bp", __name__, url_prefix="/api/users")
 
-@users_bp.route("/", methods=["GET"])
-def get_users():
-    return jsonify({"message": "Users endpoint works"})
+# Schemas
+user_schema = UserSchema()
+
+
+# --------------------------------- Endpoints ---------------------------------
+
+@users_bp.route("/", methods=["POST"])
+def create_user():
+    json_data = request.get_json()
+    if not json_data:
+        return jsonify({"message": "No input provided"}), 400
+
+    # Validation et désérialisation
+    try:
+        user = user_schema.load(json_data)
+    except ValidationError as err:
+        return jsonify({"errors": err.messages}), 422
+
+    # Création via le service
+    try:
+        user = UserService.create_user(user)
+    except ValueError as e:
+        return jsonify({"errors": str(e)}), 400
+
+    # Sérialisation de la réponse
+    result = user_schema.dump(user)
+    return jsonify(result), 201
---
+++ app/services/user_service.py
@@ -0,0 +1,22 @@
+from app.models.user import User
+from app.core.extensions import db
+from sqlalchemy.exc import IntegrityError
+
+class UserService:
+    @staticmethod
+    def create_user(user: User):
+        """
+        Crée un utilisateur et l'ajoute à la DB.
+        """
+        # Verification de doublon d'email
+        existing = User.query.filter_by(email=user.email).first()
+        if existing:
+            raise ValueError("An user with this email already exists.")
+        
+        try:
+            db.session.add(user)
+            db.session.commit()
+            return user
+        except IntegrityError as e:
+            db.session.rollback()  # Annule les changements
+            raise ValueError("Failed to create user.") from e
